0309e948c4a7adc9afc27c3e46649b34a54772f3
rwellingtonr <wellington-158@hotmail.com>
2021-12-11 17:39:41 -0300

    check existing key

--- src/Controller/sendMessageController.ts
+++ src/Controller/sendMessageController.ts
@@ -1,14 +1,15 @@
 /* What this Controller does... */
 import { Request, Response } from "express"
 import { IUserData } from "../Interface"
+import { sendMsgService } from "../Services/sendMsgService"
 
 class sendMessageController {
   async handle(req: Request, res: Response) {
     try {
       const { roomId, user }: IUserData = req.body
 
-      const service = "new Service();"
-      const result = await "service.execute()"
+      const service = new sendMsgService()
+      const result = await service.execute({ roomId, user })
       return res.status(200).json({ result: result })
     } catch (error) {
       return res.status(401).json({ error: error })
--- src/Services/sendMsgService.ts
+++ src/Services/sendMsgService.ts
@@ -2,10 +2,11 @@
 
 import { io } from "../App/app"
 import { IUserData } from "../Interface"
+import { existKey } from "../Utils/RedisMethods/retrievers"
 
 class sendMsgService {
   async execute(dataMsg: IUserData) {
-    const existIt = "await"
+    const existIt = await existKey(dataMsg.roomId)
     if (existIt) {
       // send message to an existing chat
     } else {
