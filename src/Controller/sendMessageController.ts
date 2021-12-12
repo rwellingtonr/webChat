a453851f7af5ede84e5fdd1eb61e7ce492c2c885
rwellingtonr <wellington-158@hotmail.com>
2021-12-12 14:56:49 -0300

    added logs

--- src/Controller/sendMessageController.ts
+++ src/Controller/sendMessageController.ts
@@ -9,8 +9,12 @@ class sendMessageController {
       const { roomId, user }: IUserData = req.body
 
       const service = new sendMsgService()
-      const result = await service.execute({ roomId, user })
-      return res.status(200).json({ result: result })
+      const sendingMsg = await service.execute({ roomId, user })
+      if (sendingMsg) {
+        return res.status(200).json({ message: "Message deliveried" })
+      } else {
+        return res.status(500).json({ message: "Ops... something went wrong" })
+      }
     } catch (error) {
       return res.status(401).json({ error: error })
     }
